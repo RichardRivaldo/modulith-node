e9a08749bfd8ec6b07adf13379c085ea6d375d4e
RichardRivaldo <richardrivaldo84@gmail.com>
2023-06-06 15:38:49 +0700

    feat: updated user module imports

--- modules/user/src/index.ts
+++ modules/user/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/controllers/user.controller';
 export * from './lib/controllers/auth.controller';
-export * from './lib/models/user.model';
 export * from './lib/services/user.service';
+export * from './lib/models/user.model';
+export * from './lib/types/role.enum';
