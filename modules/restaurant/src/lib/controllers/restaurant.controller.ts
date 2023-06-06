d1b9b2a19ca7d20bb0351bdd27563c72e2289555
RichardRivaldo <richardrivaldo84@gmail.com>
2023-06-06 16:45:57 +0700

    feat: implemented cuisine service, updated restaurant controller

--- modules/restaurant/src/lib/controllers/restaurant.controller.ts
+++ modules/restaurant/src/lib/controllers/restaurant.controller.ts
@@ -19,16 +19,27 @@ export class RestaurantController {
     }
 
     private setRoutes() {
+        // Restaurant endpoints
         this.router.get('/', this.getAllRestaurants);
         this.router.get('/:id', this.getRestaurantDetails);
         this.router.post('/', this.addRestaurant);
         this.router.patch('/:id', this.updateRestaurantDetails);
         this.router.delete('/:id', this.deleteRestaurant);
+
+        // Cuisine endpoints
+        const cuisineRoot = 'cuisines';
+        this.router.get(`/:id/${cuisineRoot}/`, this.getAllCuisines);
+        this.router.post(`/:id/${cuisineRoot}/`, this.addCuisine);
+
+        this.router.delete(`/${cuisineRoot}/:id`, this.deleteCuisine);
+        this.router.get(`/${cuisineRoot}/:id`, this.getCuisineDetails);
+        this.router.patch(`/${cuisineRoot}/:id`, this.updateCuisineDetails);
     }
 
+    // Restaurants
     private addRestaurant = async (req: AuthRequest, res: Response) => {
         try {
-            if (req.creds['role'] === Role.Merchant) {
+            if (req.creds['id'] && req.creds['role'] === Role.Merchant) {
                 const result = await this.restaurantService.addRestaurant(
                     req.creds['id'],
                     req.body
@@ -163,6 +174,147 @@ export class RestaurantController {
             return res.status(500).send({
                 status: 500,
                 message: `Failed deleting the restaurant! Reason: ${err.message}`,
+                data: null,
+            });
+        }
+    };
+
+    // Cuisines
+    private addCuisine = async (req: AuthRequest, res: Response) => {
+        try {
+            if (req.creds['id'] && req.creds['role'] === Role.Merchant) {
+                const result = await this.cuisineService.addCuisine(
+                    req.creds['id'],
+                    req.params.id,
+                    req.body
+                );
+                return res.status(201).send({
+                    status: 201,
+                    message: 'Successfully added a cuisine!',
+                    data: result,
+                });
+            } else {
+                return res.status(401).send({
+                    status: 401,
+                    message: 'Unauthorized action!',
+                    data: null,
+                });
+            }
+        } catch (err) {
+            return res.status(500).send({
+                status: 500,
+                message: `Failed adding new cuisine! Reason: ${err.message}`,
+                data: null,
+            });
+        }
+    };
+
+    private getAllCuisines = async (req: AuthRequest, res: Response) => {
+        try {
+            if (req.creds['id']) {
+                const result = await this.cuisineService.getAllCuisines(
+                    req.params.id
+                );
+                return res.status(200).send({
+                    status: 200,
+                    message: 'Successfully get all cuisines!',
+                    data: result,
+                });
+            } else {
+                return res.status(401).send({
+                    status: 401,
+                    message: 'Unauthorized action!',
+                    data: null,
+                });
+            }
+        } catch (err) {
+            return res.status(500).send({
+                status: 500,
+                message: `Failed getting the cuisines! Reason: ${err.message}`,
+                data: null,
+            });
+        }
+    };
+
+    private getCuisineDetails = async (req: AuthRequest, res: Response) => {
+        try {
+            if (req.creds['id']) {
+                const result = await this.cuisineService.getCuisineDetails(
+                    req.params.id
+                );
+                return res.status(200).send({
+                    status: 200,
+                    message: 'Successfully get cuisine details!',
+                    data: result,
+                });
+            } else {
+                return res.status(401).send({
+                    status: 401,
+                    message: 'Unauthorized action!',
+                    data: null,
+                });
+            }
+        } catch (err) {
+            return res.status(500).send({
+                status: 500,
+                message: `Failed getting cuisine details! Reason: ${err.message}`,
+                data: null,
+            });
+        }
+    };
+
+    private updateCuisineDetails = async (req: AuthRequest, res: Response) => {
+        try {
+            if (req.creds['id'] && req.creds['role'] === Role.Merchant) {
+                const result = await this.cuisineService.updateCuisineDetails(
+                    req.params.id,
+                    req.creds['id'],
+                    req.body
+                );
+                return res.status(200).send({
+                    status: 200,
+                    message: 'Successfully updated cuisine details!',
+                    data: result,
+                });
+            } else {
+                return res.status(401).send({
+                    status: 401,
+                    message: 'Unauthorized action!',
+                    data: null,
+                });
+            }
+        } catch (err) {
+            return res.status(500).send({
+                status: 500,
+                message: `Failed updating cuisine details! Reason: ${err.message}`,
+                data: null,
+            });
+        }
+    };
+
+    private deleteCuisine = async (req: AuthRequest, res: Response) => {
+        try {
+            if (req.creds['id'] && req.creds['role'] === Role.Merchant) {
+                const result = await this.cuisineService.deleteCuisine(
+                    req.params.id,
+                    req.creds['id']
+                );
+                return res.status(200).send({
+                    status: 200,
+                    message: 'Successfully deleted the cuisine!',
+                    data: result,
+                });
+            } else {
+                return res.status(401).send({
+                    status: 401,
+                    message: 'Unauthorized action!',
+                    data: null,
+                });
+            }
+        } catch (err) {
+            return res.status(500).send({
+                status: 500,
+                message: `Failed deleting the cuisine! Reason: ${err.message}`,
                 data: null,
             });
         }
--- modules/restaurant/src/lib/services/cuisine.service.ts
+++ modules/restaurant/src/lib/services/cuisine.service.ts
@@ -1,1 +1,95 @@
-export class CuisineService {}
+import { FilterQuery } from 'mongoose';
+import { ICuisine } from '../interfaces/cuisine.interface';
+import { Cuisine } from '../models/cuisine.model';
+import {
+    AddCuisineRequestDTO,
+    ChangeCuisineRequestDTO,
+} from '../dtos/cuisine.dto';
+import { Restaurant } from '../models/restaurant.model';
+
+export class CuisineService {
+    private filterCuisine = async (
+        query: FilterQuery<ICuisine>
+    ): Promise<ICuisine> => {
+        return await Cuisine.findOne(query);
+    };
+
+    private getRestaurantManagerID = async (id: string): Promise<string> => {
+        return (await Restaurant.findById(id)).userID.toString();
+    };
+
+    public addCuisine = async (
+        userID: string,
+        restaurantID: string,
+        cuisine: AddCuisineRequestDTO
+    ) => {
+        const managerID = await this.getRestaurantManagerID(restaurantID);
+        if (userID !== managerID) {
+            throw new Error('Unauthorized Action!');
+        }
+
+        const newCuisine = new Cuisine(cuisine);
+        newCuisine.restaurantID = restaurantID;
+        return await newCuisine.save();
+    };
+
+    public getAllCuisines = async (
+        restaurantID: string
+    ): Promise<ICuisine[]> => {
+        return await Cuisine.find({
+            restaurantID: restaurantID,
+            isActive: true,
+        }).exec();
+    };
+
+    public getCuisineDetails = async (id: string): Promise<ICuisine> => {
+        const cuisine = await this.filterCuisine({ _id: id });
+        return cuisine;
+    };
+
+    public updateCuisineDetails = async (
+        id: string,
+        userID: string,
+        data: ChangeCuisineRequestDTO
+    ): Promise<ICuisine> => {
+        const cuisine = await this.filterCuisine({ _id: id });
+        if (!cuisine) {
+            throw new Error("Cuisine with this ID doesn't exist!");
+        }
+
+        const managerID = await this.getRestaurantManagerID(
+            cuisine.restaurantID.toString()
+        );
+
+        if (userID !== managerID) {
+            throw new Error('Unauthorized Action!');
+        }
+
+        const updatedCuisine = await Cuisine.findByIdAndUpdate(
+            cuisine._id,
+            data,
+            { new: true }
+        ).exec();
+        return updatedCuisine;
+    };
+
+    public deleteCuisine = async (
+        id: string,
+        userID: string
+    ): Promise<ICuisine> => {
+        const cuisine = await this.filterCuisine({ _id: id });
+        if (!cuisine) {
+            throw new Error("Cuisine with this ID doesn't exist!");
+        }
+
+        const managerID = await this.getRestaurantManagerID(
+            cuisine.restaurantID.toString()
+        );
+
+        if (userID !== managerID) {
+            throw new Error('Unauthorized Action!');
+        }
+
+        return await cuisine.deleteOne({ new: true });
+    };
+}
